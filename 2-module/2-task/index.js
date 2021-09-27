e8dce208956cf8830dfe8bd9b88140aeb1afab98
Kirill Davletshin <prizrak41411@gmail.com>
2021-09-27 13:37:33 +0500

    решил

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
-  // ваш код...
+  let countKey = 0;
+  for (let key in obj) {
+    countKey += 1;
+  }
+  if (countKey > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
