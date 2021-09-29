7480a3f180c1cc0c24f08e9928b5509dbd0d40a4
Kirill Davletshin <prizrak41411@gmail.com>
2021-09-30 00:44:26 +0500

    нужен совет

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,17 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let hey = [];
+  let uppString = arr.map(function(item, index) {
+    
+    if (index == 0) {
+      hey.push(item);
+    } else {
+      newItem = item[0].toUpperCase() + item.slice(1);
+      hey.push(newItem);
+    }
+    return hey;
+  });
+ 
+  return hey.join('');
+  // На сколько правильно и как лучше сделать решение?  
 }
