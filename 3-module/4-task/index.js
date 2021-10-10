afdd9f05125a322066421abeda596fdb0d6dc277
Kirill Davletshin <prizrak41411@gmail.com>
2021-10-10 19:01:39 +0500

    решил

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersAgeFilter = users.filter( item => (item["age"] <= age) );
+  let resultString = usersAgeFilter.reduce( (item, current) => item + current["name"] + ", " + current["balance"] + "\n", "" );
+  return resultString.slice(0,-1);
 }
