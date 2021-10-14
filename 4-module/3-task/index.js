695950895dbf01acabac18392d02dc6f8c7f0a71
Kirill Davletshin <prizrak41411@gmail.com>
2021-10-14 12:36:43 +0500

    решил

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let tr = table.rows[i];
+
+    if (!tr.cells[3].hasAttribute('data-available')) {
+      tr.hidden = true;
+    } else if (tr.cells[3].getAttribute('data-available') == 'true') {
+      tr.classList.add('available');
+    } else {
+      tr.classList.add('unavailable');
+    }
+
+    if (tr.cells[2].innerHTML == 'm') {
+      tr.classList.add('male');
+    } else {
+      tr.classList.add('female');
+    }
+    if (tr.cells[1].innerHTML < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+  }
 }
