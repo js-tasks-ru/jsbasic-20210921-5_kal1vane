916ec9619025e5e97061ac872b056e87bd1d4f95
Kirill Davletshin <prizrak41411@gmail.com>
2021-10-14 18:13:15 +0500

    решил

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  
+  button.addEventListener('click', event => {
+    if (text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  });
+ 
 }
