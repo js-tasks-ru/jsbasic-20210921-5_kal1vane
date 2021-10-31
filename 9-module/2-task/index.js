f9845735a37a97f3153667b7c0210616ffcd8d2e
Kirill Davletshin <prizrak41411@gmail.com>
2021-11-01 01:16:29 +0500

    v1.0

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,68 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    Promise.all([this.carouselAdd(), this.ribbonMenuAdd(), this.sliderStepsAdd(), this.cartIconAdd()]);
+
+    this.products = await this.fetchProducts();
+    await this.productGridAdd();
+
+    document.body.addEventListener('product-add', (event) => {
+      let product = this.products.find(item => item.id == event.detail);
+      this.cart.addProduct(product);
+    });
+    this.stepSlider.elem.addEventListener('slider-change', (event) => {
+      this.productsGrid.updateFilter({
+        maxSpiciness: event.detail
+      });
+    });
+    this.ribbon.elem.addEventListener('ribbon-select', (event) => {
+      this.productsGrid.updateFilter({
+        category: event.detail
+      });
+    });
+    document.getElementById('nuts-checkbox').onchange = (event) => {
+      this.productsGrid.updateFilter({
+        noNuts: event.target.checked
+      });
+    };
+    document.getElementById('vegeterian-checkbox').onchange = (event) => {
+      this.productsGrid.updateFilter({
+        vegeterianOnly: event.target.checked
+      });
+    };
+  }
+  carouselAdd() {
+    let carousel = new Carousel(slides);
+    let containerCarousel = document.body.querySelector(`[data-carousel-holder]`);
+    containerCarousel.append(carousel.elem);
+  }
+  ribbonMenuAdd() {
+    this.ribbon = new RibbonMenu(categories);
+    let container = document.querySelector(`[data-ribbon-holder]`);
+    container.append(this.ribbon.elem);
+  }
+  sliderStepsAdd() {
+    this.stepSlider = new StepSlider({
+      steps: 5,
+      value: 3
+    });
+    let container = document.querySelector(`[data-slider-holder]`);
+    container.append(this.stepSlider.elem);
+  }
+  cartIconAdd() {
+    this.cartIcon = new CartIcon();
+    let cartIconHolder = document.querySelector('[data-cart-icon-holder]');
+    cartIconHolder.append(this.cartIcon.elem);
+    this.cart = new Cart(this.cartIcon);
+  }
+  productGridAdd() {
+    this.productsGrid = new ProductsGrid(this.products);
+    let container = document.querySelector('[data-products-grid-holder]');
+    container.innerHTML = "";
+    container.append(this.productsGrid.elem);
+  }
+  async fetchProducts() {
+    let products = await fetch("products.json");
+    return await products.json();
   }
 }
